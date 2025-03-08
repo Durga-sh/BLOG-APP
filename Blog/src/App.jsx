df3ea3bc440c946f4c4575157a102ba03f635b44
Durga1234-cloud <puhandurgasankarp@gmail.com>
2025-03-08 23:02:18 +0530

    Add Component

--- Blog/src/App.jsx
+++ Blog/src/App.jsx
@@ -1,11 +1,44 @@
-
-
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
-
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
 function App() {
+  // const [count, setCount] = useState(0)
+const { setLoading} = useState(true)
+const dispatch = useDispatch()
+
+useEffect(()=>{
+  authService .getCurrentUser()
+
+  .then((userData) =>{
+    if (userData) {
+      dispatch(login({userData}))
+      
+    }
+    else{
+      dispatch(logout())
+    }
+  })
+  .catch()
+  .finally(()=> setLoading(false))
+} , [])
   return (
     <>
-      
+     
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        {/* <main>
+        TODO:  <Outlet />
+        </main> */}
+        <Footer />
+      </div>
+    </div>
+
+
     </>
   )
 }
---
+++ Blog/src/components/index.js
@@ -0,0 +1,7 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/container";
+import Logo from "./logo";
+import LogoutBtn from "./Header/Logoutbtn";
+
+export {Header , Footer , Logo , LogoutBtn , Container}
