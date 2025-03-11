9b413f02cb9498b9d9e5244fb5f27faf5bc7c5b4
Durga1234-cloud <puhandurgasankarp@gmail.com>
2025-03-11 23:41:26 +0530

    Add Pages

--- Blog/src/components/index.js
+++ Blog/src/components/index.js
@@ -3,5 +3,11 @@ import Footer from "./Footer/Footer";
 import Container from "./container/container";
 import Logo from "./logo";
 import LogoutBtn from "./Header/Logoutbtn";
+import Input from "./input";
+import Signup from "./signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
 
-export {Header , Footer , Logo , LogoutBtn , Container}
+export {Header , Footer , Logo , LogoutBtn , Container , Input , Signup , Login , PostForm , PostCard , AuthLayout}
